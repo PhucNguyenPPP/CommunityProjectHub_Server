04f38a2c217b77f00782f41d64d7c4722fa198f2
nghthtam <thanhtamthd3@gmail.com>
2025-02-10 08:09:12 +0700

    [Add]_Function_Search&Paging_GetAllAccount

---
+++ CPH.Common/DTO/Account/ListAccountDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CPH.Common.DTO.Paging;
+using CPH.Common.DTO.Project;
+
+namespace CPH.Common.DTO.Account
+{
+    public class ListAccountDTO
+    {
+        public int? CurrentPage { get; set; }
+        public int? RowsPerPages { get; set; }
+        public int? TotalCount { get; set; }
+        public int? TotalPages { get; set; }
+        public PagedList<AccountResponseDTO>? AccountResponseDTOs { get; set; }
+    }
+}
