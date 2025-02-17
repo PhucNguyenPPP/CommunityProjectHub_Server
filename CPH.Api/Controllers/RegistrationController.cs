49fa53cf97ca0e78f6f504f3ffaa9b7deb726dc1
anhthien8933@gmail.com <anhthien8933@gmail.com>
2025-02-17 13:07:18 +0700

    commit

---
+++ CPH.Api/Controllers/RegistrationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CPH.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+    }
+}
