8d70c939ce9b45e59c58a151639f86669d416f6f
anhthien8933@gmail.com <anhthien8933@gmail.com>
2025-02-18 22:16:05 +0700

    Add GetAllSentRegistrations Fix SendRegistration

--- CPH.BLL/Services/RegistrationService.cs
+++ CPH.BLL/Services/RegistrationService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,11 +8,15 @@ using AutoMapper;
 using CPH.BLL.Interfaces;
 using CPH.Common.Constant;
 using CPH.Common.DTO.General;
+using CPH.Common.DTO.Paging;
+using CPH.Common.DTO.Project;
 using CPH.Common.DTO.Registration;
 using CPH.Common.Enum;
 using CPH.DAL.Entities;
 using CPH.DAL.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CPH.BLL.Services
 {
@@ -38,10 +43,10 @@ namespace CPH.BLL.Services
                 re.Status = RegistrationStatusConstant.Canceled;
                 _unitOfWork.Registration.Update(re);
                 var canceled = await _unitOfWork.SaveChangeAsync();
-                if(canceled)
+                if (canceled)
                 {
                     return new ResponseDTO("Huỷ đơn đăng ký thành công", 200, true);
-                }    
+                }
             }
             return new ResponseDTO("Huỷ đơn đăng ký thất bại", 500, true);
 
@@ -58,17 +63,17 @@ namespace CPH.BLL.Services
             {
                 return new ResponseDTO("Đơn đăng ký không tồn tại", 404, false);
             }
-            if(!re.Status.Equals(RegistrationStatusConstant.Processing))
+            if (!re.Status.Equals(RegistrationStatusConstant.Processing))
             {
                 return new ResponseDTO("Đơn đăng ký không thể huỷ", 400, false);
             }
-            return new ResponseDTO("Hợp lệ", 200, true,re);
+            return new ResponseDTO("Hợp lệ", 200, true, re);
         }
 
         public async Task<ResponseDTO> SendRegistration(SendRegistrationDTO registrationDTO)
         {
             var check = await CheckRegistrationValid(registrationDTO);
-            if(check.IsSuccess==false)
+            if (check.IsSuccess == false)
             {
                 return check;
             }
@@ -78,7 +83,7 @@ namespace CPH.BLL.Services
             registration.CreatedAt = DateTime.Now;
             await _unitOfWork.Registration.AddAsync(registration);
             var added = await _unitOfWork.SaveChangeAsync();
-            if(added)
+            if (added)
             {
                 return new ResponseDTO("Đăng ký thành công", 200, true);
             }
@@ -86,7 +91,7 @@ namespace CPH.BLL.Services
         }
 
         private async Task<ResponseDTO> CheckRegistrationValid(SendRegistrationDTO registrationDTO)
-        {           
+        {
             List<string> listError = new List<string>();
 
             var cl = await _unitOfWork.Class.GetByCondition(c => c.ClassId.Equals(registrationDTO.ClassId));
@@ -116,9 +121,9 @@ namespace CPH.BLL.Services
                         }
                         else
                         {
-                           var regis = await _unitOfWork.Registration.GetByCondition(r=>r.AccountId.Equals(registrationDTO.AccountId) 
-                           && r.ClassId.Equals(registrationDTO.ClassId) 
-                           && (r.Status.Equals(RegistrationStatusConstant.Processing)||r.Status.Equals(RegistrationStatusConstant.Inspected)));
+                            var regis = await _unitOfWork.Registration.GetByCondition(r => r.AccountId.Equals(registrationDTO.AccountId)
+                            && r.ClassId.Equals(registrationDTO.ClassId)
+                            && (r.Status.Equals(RegistrationStatusConstant.Processing) || r.Status.Equals(RegistrationStatusConstant.Inspected)));
                             if (regis != null)
                             {
                                 listError.Add("Bạn đã từng đăng ký tham gia lớp học này");
@@ -139,9 +144,9 @@ namespace CPH.BLL.Services
                                             {
                                                 if (lscOfAccRegistered[j].StartTime != null && lscOfAccRegistered[j].EndTime != null)
                                                 {
-                                                    var checklsc = lscToRegister.Where(t => !((t.StartTime < lscOfAccRegistered[j].StartTime
-                                                    && t.EndTime < lscOfAccRegistered[j].StartTime) || (t.StartTime > lscOfAccRegistered[j].EndTime && t.EndTime > lscOfAccRegistered[j].EndTime)));
-                                                    if (checklsc != null)
+                                                    var checklsc = lscToRegister.Where(t => !(t.StartTime < lscOfAccRegistered[j].StartTime
+                                                    && t.EndTime < lscOfAccRegistered[j].StartTime || t.StartTime > lscOfAccRegistered[j].EndTime && t.EndTime > lscOfAccRegistered[j].EndTime));
+                                                    if (checklsc.Count()>0)
                                                     {
                                                         var c = await _unitOfWork.Class.GetByCondition(c => c.ClassId.Equals(lscOfAccRegistered[j].ClassId));
                                                         var l = await _unitOfWork.Lesson.GetByCondition(c => c.LessonId.Equals(lscOfAccRegistered[j].LessonId));
@@ -225,11 +230,111 @@ namespace CPH.BLL.Services
             {
                 return new ResponseDTO("Không thể trả lời đơn đăng ký này", 400, false);
             }
-            if (answerRegistrationDTO.Type!="Approve" && answerRegistrationDTO.Type!="Deny")
+            if (answerRegistrationDTO.Type != "Approve" && answerRegistrationDTO.Type != "Deny")
             {
                 return new ResponseDTO("Phần trả lời đơn đăng ký không hợp lệ", 400, false);
             }
-            return new ResponseDTO("Thông tin trả lời đơn đăng ký hợp lệ", 200, true,re);
+            return new ResponseDTO("Thông tin trả lời đơn đăng ký hợp lệ", 200, true, re);
+        }
+
+        public async Task<ResponseDTO> GetAllSentRegistrations(Guid accountId, string search, int? rowsPerPage, int? pageNumber)
+        {
+            List<SentRegistDTO>? listDTO = new List<SentRegistDTO>();
+            var check = await CheckAccountToGetRegistrations(accountId);
+            if (!check.IsSuccess)
+            {
+                return check;
+            }
+            var regis = _unitOfWork.Registration.GetAllByCondition(r => r.AccountId.Equals(accountId)).Include(r => r.Class).ToList();
+            listDTO = _mapper.Map<List<SentRegistDTO>>(regis);
+            if (listDTO == null)
+            {
+                return new ResponseDTO("Tài khoản không có đơn đăng ký", 404, false);
+            }
+           
+            for (int i = 0; i < listDTO.Count; i++)
+            {
+                    var projectTtile = await _unitOfWork.Project.GetByCondition(c => c.ProjectId.Equals(listDTO[i].ProjectId)) ;
+                    if (projectTtile != null)
+                    {
+                 
+                        listDTO[i].Title = projectTtile.Title;
+                    }
+                }
+            
+            
+            if (search.IsNullOrEmpty() && pageNumber == null && rowsPerPage == null)
+            {
+
+                return new ResponseDTO("Hiển thị thông tin các đơn đăng ký của tài khoản hợp lệ", 200, true, listDTO);
+            }
+            else
+            {
+                if (search != null)
+                {
+                    if (listDTO != null) // Kiểm tra null trước khi lọc
+                    {
+                        listDTO = listDTO.Where(c =>
+                            (!string.IsNullOrEmpty(c.Title) && c.Title.ToLower().Contains(search.ToLower())) ||
+                            (!string.IsNullOrEmpty(c.ClassCode) && c.ClassCode.ToLower().Contains(search.ToLower()))
+
+                        ).ToList();
+                    }
+                }
+                if (listDTO==null || !listDTO.Any())
+                {
+                    return new ResponseDTO("Không có đơn đăng ký trùng khớp", 404, false);
+                }
+
+                if (pageNumber == null && rowsPerPage != null)
+                {
+                    return new ResponseDTO("Vui lòng chọn số trang", 400, false);
+                }
+                if (pageNumber != null && rowsPerPage == null)
+                {
+                    return new ResponseDTO("Vui lòng chọn số dòng mỗi trang", 400, false);
+                }
+                if (pageNumber <= 0 || rowsPerPage <= 0)
+                {
+                    return new ResponseDTO("Giá trị phân trang không hợp lệ", 400, false);
+                }
+
+ 
+                if (pageNumber != null && rowsPerPage != null)
+                {
+                    var pagedList = PagedList<SentRegistDTO>.ToPagedList(listDTO.AsQueryable(), pageNumber, rowsPerPage);
+                    var result = new ListSentRegisDTO
+                    {
+                        Registered = pagedList,
+                        CurrentPage = pageNumber,
+                        RowsPerPages = rowsPerPage,
+                        TotalCount = listDTO.Count,
+                        TotalPages = (int)Math.Ceiling(listDTO.Count / (double)rowsPerPage)
+                    };
+                    return new ResponseDTO("Tìm kiếm đơn đăng ký thành công", 200, true, result);
+                }
+                return new ResponseDTO("Tìm kiếm dự án thành công", 200, true, listDTO);
+
+            }
+        }
+
+
+        private async Task<ResponseDTO> CheckAccountToGetRegistrations(Guid accountId)
+        {
+            if (accountId.ToString().IsNullOrEmpty())
+            {
+                return new ResponseDTO("Vui lòng nhập Id của tài khoản", 400, false);
+            }
+            var acc = await _unitOfWork.Account.GetByCondition(a => a.AccountId.Equals(accountId));
+            if (acc == null)
+            {
+                return new ResponseDTO("Tài khoản không tồn tại", 404, false);
+            }
+            if (!acc.RoleId.Equals((int)RoleEnum.Student) && !acc.RoleId.Equals((int)RoleEnum.Lecturer))
+            {
+                return new ResponseDTO("Tài khoản không thể nộp đơn đăng ký", 400, false);
+            }
+            return new ResponseDTO("Id tài khoản hợp lệ", 200, true);
         }
     }
 }
