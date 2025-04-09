f09bcffb11164225a2f35787924b93f66711cbe0
nghthtam <thanhtamthd3@gmail.com>
2025-04-09 10:31:14 +0700

    [Scaffold]

---
+++ CPH.DAL/Entities/Question.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CPH.DAL.Entities;
+
+public partial class Question
+{
+    public Guid QuestionId { get; set; }
+
+    public string QuestionContent { get; set; } = null!;
+
+    public Guid FormId { get; set; }
+
+    public virtual ICollection<Answer> Answers { get; set; } = new List<Answer>();
+
+    public virtual Form Form { get; set; } = null!;
+}
