3f2cf825ae1c295114610fcd27b679e6dbc39af2
nghthtam <thanhtamthd3@gmail.com>
2025-04-09 12:14:21 +0700

    [Add]_Function_GetAllQuestion

---
+++ CPH.Api/Controllers/QuestionController.cs
@@ -0,0 +1,38 @@
+﻿using CPH.BLL.Interfaces;
+using CPH.BLL.Services;
+using CPH.Common.DTO.General;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CPH.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        private readonly IQuestionService _questionService;
+
+        public QuestionController(IQuestionService questionService)
+        {
+            _questionService = questionService;
+        }
+
+        [HttpGet("all-question-of-project")]
+        public async Task<IActionResult> GetAllQuestion()
+        {
+            ResponseDTO responseDTO = await _questionService.GetAllQuestion();
+            if (responseDTO.IsSuccess == false)
+            {
+                if (responseDTO.StatusCode == 400)
+                {
+                    return NotFound(responseDTO);
+                }
+                if (responseDTO.StatusCode == 500)
+                {
+                    return BadRequest(responseDTO);
+                }
+            }
+            return Ok(responseDTO);
+        }
+    }
+}
---
+++ CPH.DAL/Repositories/AnswerRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CPH.DAL.Context;
+using CPH.DAL.Entities;
+using CPH.DAL.Interfaces;
+
+namespace CPH.DAL.Repositories
+{
+    public class AnswerRepository : GenericRepository<Answer>, IAnswerRepository
+    {
+        public AnswerRepository(CphDbContext context) : base(context)
+        {
+            
+        }
+    }
+}
