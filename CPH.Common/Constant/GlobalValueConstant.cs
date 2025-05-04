d93986afdd0136c03eae5600f3f63c1c369cdaa3
NguyenPhucPPP <nguyenphucdt36@gmail.com>
2025-05-04 17:17:44 +0700

    Add maximum time for feedback constant

---
+++ CPH.Common/Constant/GlobalValueConstant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPH.Common.Constant
+{
+    public class GlobalValueConstant
+    {
+        public const string MaximumTimeForFeedbackConstantName = "MAXIMUM_TIME_FOR_FEEDBACK";
+    }
+}
