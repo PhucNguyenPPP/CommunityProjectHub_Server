ba733fa4cac7ae0acac45b8d953931c83136a24d
NguyenPhucPPP <nguyenphucdt36@gmail.com>
2025-05-04 20:45:13 +0700

    [Add]_Get all feedback of project api

---
+++ CPH.Common/DTO/Feedback/GetAllFeedbackOfProjectResponseDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPH.Common.DTO.Feedback
+{
+    public class GetAllFeedbackOfProjectResponseDTO
+    {
+        public Guid TraineeId { get; set; }
+        public string TraineeAccountCode { get; set; } = null!;
+        public string TraineeName { get; set; } = null!;
+        public string? FeedbackContent { get; set; }
+        public DateTime? FeedbackCreatedDate { get; set; }
+        public List<TraineeFeedbackAnswer> traineeFeedbackAnswers { get; set; } = new List<TraineeFeedbackAnswer>();
+    }
+}
